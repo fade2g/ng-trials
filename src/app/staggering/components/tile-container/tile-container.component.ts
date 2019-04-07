a6bd2ea12a5e447b7a2d48041152e1da48ea0b4e
Holger Heymanns <holger.heymanns@gmail.com>
2019-04-07 21:58:54 +0200

    Some styling of the staggerings tiles

--- src/app/staggering/components/tile-container/tile-container.component.ts
+++ src/app/staggering/components/tile-container/tile-container.component.ts
@@ -12,9 +12,9 @@ import {animate, query, stagger, style, transition, trigger} from '@angular/anim
     trigger('pageAnimations', [
       transition(':enter', [
         query('.tile', [
-          style({opacity: 0, transform: 'translateY(-100px)'}),
-          stagger(30, [
-            animate('1500ms cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1, transform: 'none'}))
+          style({opacity: 0, transform: 'scaleY(0)', 'transform-origin': 'top left'}),
+          stagger(60, [
+            animate('500ms cubic-bezier(0.35, 0, 0.25, 1)', style({opacity: 1, transform: 'none'}))
           ])
         ])
       ])
